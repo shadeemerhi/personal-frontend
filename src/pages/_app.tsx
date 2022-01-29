89dc08304699dedb53a70970519e774feeb27856
Shadee Merhi <shadmerhi@gmail.com>
2022-01-29 13:18:52 -0700

    restructure folders, add layout

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app'
+import Layouts from '../components/Layouts';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <Layouts>
+          <Component {...pageProps} />
+      </Layouts>
+  );
+}
+
+export default MyApp
