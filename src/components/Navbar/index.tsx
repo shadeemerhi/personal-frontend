370b273696a0ec5c239075fc9bd4d27406d3caf3
Shadee Merhi <shadmerhi@gmail.com>
2022-01-29 17:11:53 -0700

    navbar desktop selected

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,40 @@
 import React, { useState } from "react";
 
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import ArticleIcon from "@mui/icons-material/Article";
+import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import { Icon } from "@mui/material";
+
 import styles from "../../styles/Navbar.module.scss";
 import classNames from "classnames";
 
-interface NavbarProps {}
+const navItems = [
+    {
+        text: "Home",
+        icon: <HomeIcon />
+    },
+    {
+        text: "Projects",
+        icon: <CodeIcon />
+    },
+    {
+        text: "Experience",
+        icon: <WorkOutlineIcon />
+    },
+    {
+        text: "Blog",
+        icon: <ArticleIcon />
+    },
+    {
+        text: "Contact",
+        icon: <AlternateEmailIcon />
+    },
+];
 
-const navItems = ["Home", "Projects", "Experience", "Contact"];
-
-const Navbar: React.FC<NavbarProps> = () => {
-    const [selected, setSelected] = useState(navItems[0]);
+const Navbar: React.FC<{}> = () => {
+    const [selected, setSelected] = useState(navItems[0].text);
     return (
         <div className={styles.root}>
             {navItems.map((item) => (
@@ -16,11 +42,12 @@ const Navbar: React.FC<NavbarProps> = () => {
                     className={classNames({
                         pointer: true,
                         [styles.nav_item]: true,
-                        [styles.__selected]: item === selected,
+                        [styles._selected]: item.text === selected
                     })}
-                    onClick={() => setSelected(item)}
+                    onClick={() => setSelected(item.text)}
                 >
-                    {item}
+                    {item.icon && <Icon className={styles.icon}>{item.icon}</Icon>}
+                    {item.text}
                 </div>
             ))}
         </div>
