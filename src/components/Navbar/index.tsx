23ad7f70bc7ea4ff5efc6b2dae49e3fe02a281d6
shadeemerhi <shadmerhi@gmail.com>
2022-02-07 11:06:26 -0700

    add lock item container

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ import CodeIcon from "@mui/icons-material/Code";
 import ArticleIcon from "@mui/icons-material/Article";
 import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
-import { Box, Grid, Icon } from "@mui/material";
+import LockIcon from "@mui/icons-material/Lock";
 
 import styles from "../../styles/Navbar.module.scss";
 import classNames from "classnames";
@@ -13,53 +13,56 @@ import NavItem, { NavItemInputs } from "./NavItem";
 import { useRouter } from "next/router";
 
 const navItems = [
-    {
-        text: "Home",
-        route: "/",
-        icon: HomeIcon,
-    },
-    {
-        text: "Projects",
-        route: "/projects",
-        icon: CodeIcon,
-    },
-    {
-        text: "Experience",
-        route: "/experience",
-        icon: WorkOutlineIcon,
-    },
-    {
-        text: "Blog",
-        route: "/blog",
-        icon: ArticleIcon,
-    },
-    {
-        text: "Contact",
-        route: "/contact",
-        icon: AlternateEmailIcon,
-    },
+  {
+    text: "Home",
+    route: "/",
+    icon: HomeIcon,
+  },
+  {
+    text: "Projects",
+    route: "/projects",
+    icon: CodeIcon,
+  },
+  {
+    text: "Experience",
+    route: "/experience",
+    icon: WorkOutlineIcon,
+  },
+  {
+    text: "Blog",
+    route: "/blog",
+    icon: ArticleIcon,
+  },
+  {
+    text: "Contact",
+    route: "/contact",
+    icon: AlternateEmailIcon,
+  },
 ];
 
 const Navbar: React.FC<{}> = () => {
-    const router = useRouter();
-    console.log("HERE IS THE ROUTER", router);
+  const router = useRouter();
+  const initSelected =
+    navItems.find((item) => item.route === router.pathname)?.text || "";
 
-    const initSelected =
-        navItems.find((item) => item.route === router.pathname)?.text || "";
-
-    const [selected, setSelected] = useState(initSelected);
-    return (
-        <div className={styles.root}>
-            {navItems.map((item, index) => (
-                <NavItem
-                    key={index}
-                    item={item}
-                    isSelected={item.text === selected}
-                    setSelected={setSelected}
-                    width={Math.round((1 / navItems.length) * 100)}
-                />
-            ))}
-        </div>
-    );
+  const [selected, setSelected] = useState(initSelected);
+  return (
+    <div className={styles.root}>
+      <div className={styles.items_container}>
+        {navItems.map((item, index) => (
+          <NavItem
+            key={index}
+            item={item}
+            isSelected={item.text === selected}
+            setSelected={setSelected}
+            width={Math.round((1 / navItems.length) * 100)}
+          />
+        ))}
+      </div>
+      <div className={styles.lock_icon_container}>
+        <LockIcon className={`${styles.icon} pointer`} />
+      </div>
+    </div>
+  );
 };
 export default Navbar;
