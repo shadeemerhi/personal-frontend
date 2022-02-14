dc22cbac8fbc408388363f6177fd86dc55961e15
shadeemerhi <shadmerhi@gmail.com>
2022-02-14 13:13:22 -0700

    add form skeleton

--- src/components/Experience/Education/Form.tsx
+++ src/components/Experience/Education/Form.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { EducationFormState } from ".";
+import { EducationItem } from "../../../generated/graphql";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Alert, Box, CircularProgress } from "@mui/material";
-import InputField from "../../Project/InputField";
 
-import { EducationItem } from "../../../generated/graphql";
+import InputField from "../../Project/InputField";
+import DateInputs from "../../Project/DateInputs";
+
+import styles from "../../Project/ProjectForm.module.scss";
 
 type FormProps = {
   setShowForm: (value: any) => void;
@@ -33,6 +36,8 @@ const Form: React.FC<FormProps> = ({
     }));
   };
 
+  const onSubmit = () => {};
+
   return (
     <>
       <ArrowBackIcon
@@ -44,6 +49,88 @@ const Form: React.FC<FormProps> = ({
           }))
         }
       />
+      <Box className={`${styles.outer_form_container} custom_form`} mb={10}>
+        <h3 className="heavy_text">
+          {false ? "Update Project" : "Create New Work Item"}
+        </h3>
+        <Box>
+          <Box
+            className={styles.input_container}
+            display="flex"
+            flexDirection="column"
+          >
+            <InputField
+              name="schoolName"
+              handleChange={handleChange}
+              label="School Name"
+              placeholder="School Name"
+              value={currentItem.schoolName}
+            />
+          </Box>
+          <Box
+            className={styles.input_container}
+            display="flex"
+            flexDirection="column"
+          >
+            <InputField
+              name="title"
+              handleChange={handleChange}
+              label="Title"
+              placeholder="Title"
+              value={currentItem.title}
+            />
+          </Box>
+          <DateInputs
+            startDate={currentItem.startDate}
+            endDate={currentItem.endDate}
+            inProgress={currentItem.inProgress}
+            handleChange={handleChange}
+          />
+        </Box>
+        {false && (
+          <Box mb={2} mt={2}>
+            <Alert severity="success">
+              {`Item successfully ${true ? "created" : "updated"}`}
+              {". "}
+              <span
+                className="heavy_text pointer"
+                onClick={() =>
+                  setShowForm((prev: EducationFormState) => ({
+                    ...prev,
+                    visible: false,
+                  }))
+                }
+              >
+                View
+              </span>
+            </Alert>
+          </Box>
+        )}
+        {(false || incompleteItem) && (
+          <Box mb={2} mt={2}>
+            <Alert severity="error">
+              {incompleteItem
+                ? "One or more of the required fields is missing"
+                : "Error creating/updating project"}
+            </Alert>
+          </Box>
+        )}
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          className={styles.submit_container}
+        >
+          <button className="btn_primary submit_button" onClick={onSubmit}>
+            {false ? (
+              <CircularProgress size={18} color="inherit" />
+            ) : (
+              <>{editing ? "Save Item" : "Create Item"}</>
+            )}
+          </button>
+        </Box>
+      </Box>
     </>
   );
 };
